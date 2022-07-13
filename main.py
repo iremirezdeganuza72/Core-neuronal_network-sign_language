a9a4b69c4f214f117b01646abd5f4ccdff4fce2c
inigo <iremirezdeganuza@gmail.com>
2022-07-13 23:15:07 +0200

    realizando fproject

--- main.py
+++ main.py
@@ -1,40 +1,65 @@
+import os
+import numpy as np
 import cv2
 import mediapipe as mp
+from os import listdir
 mp_drawing = mp.solutions.drawing_utils
 mp_drawing_styles = mp.solutions.drawing_styles
 mp_hands = mp.solutions.hands
 
-cap = cv2.VideoCapture(0)
+# For static images:
+HEIGHT= 200
+WIDTH= 200
+# Indicate the path to access the directory
+data_path="data"
+data= os.listdir(data_path)
+# Run each file through the path and save it in "sign_language"
+# Run each image through the path then save it in IMAGE_FILES
+IMAGE_FILES=[]
+limite = 1
+for folder in data:
+  contador=0
+  sign_language= os.listdir(f"{data_path}/{folder}")
+  for images in sign_language:
+    if contador < limite:
+      IMAGE_FILES.append(f"{data_path}/{folder}/{images}")
+      contador+= 1
+print(len(IMAGE_FILES))
 with mp_hands.Hands(
-    model_complexity=0,
-    min_detection_confidence=0.5,
-    min_tracking_confidence=0.5) as hands:
-  while cap.isOpened():
-    success, image = cap.read()
-    if not success:
-      print("Ignoring empty camera frame.")
-      # If loading a video, use 'break' instead of 'continue'.
+    static_image_mode=True,
+    max_num_hands=2,
+    min_detection_confidence=0.5) as hands:
+  for idx, file in enumerate(IMAGE_FILES):
+    # Read an image, flip it around y-axis for correct handedness output (see
+    # above).
+    image = cv2.flip(cv2.imread(file), 1)
+    # Convert the BGR image to RGB before processing.image.png
+    results = hands.process(cv2.cvtColor(image, cv2.COLOR_BGR2RGB))
+
+    # Print handedness and draw hand landmarks on the image.
+   
+    if not results.multi_hand_landmarks:
       continue
-
-    # To improve performance, optionally mark the image as not writeable to
-    # pass by reference.
-    image.flags.writeable = False
-    image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
-    results = hands.process(image)
-
-    # Draw the hand annotations on the image.
-    image.flags.writeable = True
-    image = cv2.cvtColor(image, cv2.COLOR_RGB2BGR)
-    if results.multi_hand_landmarks:
-      for hand_landmarks in results.multi_hand_landmarks:
-        mp_drawing.draw_landmarks(
-            image,
-            hand_landmarks,
-            mp_hands.HAND_CONNECTIONS,
-            mp_drawing_styles.get_default_hand_landmarks_style(),
-            mp_drawing_styles.get_default_hand_connections_style())
-    # Flip the image horizontally for a selfie-view display.
-    cv2.imshow('MediaPipe Hands', cv2.flip(image, 1))
-    if cv2.waitKey(5) & 0xFF == 27:
+    for hand_landmarks in results.multi_hand_landmarks:
+      print (hand_landmarks.landmark[0].x, dir(hand_landmarks.landmark[0]))
       break
-cap.release()
+      #print('hand_landmarks:', hand_landmarks)
+      #print(
+          #f'Index finger tip coordinates: (',
+          #f'{hand_landmarks.landmark[mp_hands.HandLandmark.INDEX_FINGER_TIP].x * image_width}, '
+          #f'{hand_landmarks.landmark[mp_hands.HandLandmark.INDEX_FINGER_TIP].y * image_height})'
+      #)
+      #mp_drawing.draw_landmarks(
+          #annotated_image,
+          #hand_landmarks,
+          #mp_hands.HAND_CONNECTIONS,
+          #mp_drawing_styles.get_default_hand_landmarks_style(),
+          #mp_drawing_styles.get_default_hand_connections_style())
+    #cv2.imwrite(
+        #'/tmp/annotated_image' + str(idx) + '.png', cv2.flip(annotated_image, 1))
+    # Draw hand world landmarks.
+    #if not results.multi_hand_world_landmarks:
+    # continue
+    # for hand_world_landmarks in results.multi_hand_world_landmarks:
+    # mp_drawing.plot_landmarks(
+    # hand_world_landmarks, mp_hands.HAND_CONNECTIONS, azimuth=5)
